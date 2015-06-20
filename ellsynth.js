81121ef84a64a52509ff1a61b8af6628f204d733
Colin Clark <colinbdclark@gmail.com>
2015-06-19 21:30:25 -0400

    Ensures the synth doesn't play on creation. Binds this stop button to the synth's pause method. Binds the slider's slide event to the synth's frequency input.

--- ellsynth.js
+++ ellsynth.js
@@ -1,10 +1,9 @@
-
-
 var synth = flock.synth({
     synthDef: {
-      id:"mySynth",
+        id:"mySynth",
         ugen: "flock.ugen.sinOsc",
         freq: 440,
         mul: 0.25
-    }
+    },
+    addToEnvironment: false // This tells Flocking not to start playing this synth right away.
 });
