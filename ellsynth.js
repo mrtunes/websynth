1f163a281b0be9604b897ffb19c8faa3bc3a83b6
Elliott Fienberg <mrtunes@gmail.com>
2015-07-08 07:52:34 -0400

    added the gate, removed the need for play and stop buttons

--- ellsynth.js
+++ ellsynth.js
@@ -2,10 +2,16 @@ var synth = flock.synth({
     synthDef: {
         id:"mySynth",
         ugen: "flock.ugen.sinOsc",
-        mul: 180,
-        
+            
         freq: 440,
-        mul: 0.25
+        mul: {  id:"mod",
+                ugen: "flock.ugen.env.simpleASR",
+                gate: 0,
+                attack: 0.25,
+                sustain: 1.0,
+                release: 0.5
+            }
+        
     },
     addToEnvironment: false // This tells Flocking not to start playing this synth right away.
 });
